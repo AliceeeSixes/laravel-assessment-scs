85a0668b67c90286453ab6f1753e99d3bd37f2a7
AliceeeSixes <6d6perturn@gmail.com>
2025-06-05 12:14:50 +0100

    search bar focus styling

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -1,5 +1,5 @@
 @props(["action"=>"/"])
-<form id="search" action="{{ $action }}" method="get" class="flex justify-center mb-10 text-xl text-black">
+<form id="search" action="{{ $action }}" method="get" class="flex justify-center w-fit rounded-full m-auto mb-10 text-xl text-black border border-2 border-transparent focus-within:border-sky-500 dark:focus-within:border-fuchsia-800">
     @php
         if (isset($_GET["q"])) {
             $query = htmlspecialchars($_GET["q"]);
@@ -7,6 +7,6 @@
             $query = "";
         }
     @endphp
-    <input name="q" class="bg-gray-200 rounded-l-full px-3 py-1 font-xl w-md" value="{{ $query }}"/>
+    <input name="q" class="bg-gray-200 rounded-l-full px-3 py-1 font-xl w-md focus-within:outline-none" value="{{ $query }}"/>
     <x-button class="rounded-l-none" colour="blue" type="submit"><i class="fa fa-search px-1"></i></x-button>
 </form>
